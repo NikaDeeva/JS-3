56a90f6800e510faf5ae87fa3e7c5f050be00913
NikaDeeva <136634291+NikaDeeva@users.noreply.github.com>
2024-11-09 11:49:19 +0100

    ok

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict"
+const options = {
+        rootMargin: '0px',
+        threshold: 0.15,
+    };
+    const onEntry = (entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting){
+                const image = entry.target.querySelector('.image');
+                image.src = image.dataset.src;
+                observer.unobserve(entry.target);
+            }
+        })
+    };
+    document.addEventListener('DOMContentLoaded', () => {
+        const images = document.querySelectorAll('.image');
+        const observer = new IntersectionObserver(onEntry, options);
+        images.forEach(image => {
+            observer.observe(image);
+        })
+    });
+    
